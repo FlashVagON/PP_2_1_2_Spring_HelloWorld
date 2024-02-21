76234ee82e931beed539d6ddab66011f6b09b5d8
FlashVagON <djvagon@mail.ru>
2024-02-21 21:06:57 +0500

    bean Cat

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,14 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 public class HelloWorld {
- 
+
     private String message;
- 
+
     public String getMessage() {
         return message;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
